40653b6fd4fd1cd35779538cfa858e5e916f5dc9
Hernan Thiers <hernanthiers@gmail.com>
2015-10-29 22:03:21 -0300

    Prueba: Excel.

--- controllers/TasksController.php
+++ controllers/TasksController.php
@@ -1421,17 +1421,11 @@ class TasksController extends ControllerBase
         // cols
         $colArray = array (
             0 => 'B',
-            1 => 'C',
-            2 => 'D',
-            3 => 'E',
-            4 => 'F',
-            5 => 'G',
-            6 => 'H',
-            7 => 'I',
-            8 => 'J',
-            9 => 'K',
-            10 => 'L',
-            11 => 'M'
+            3 => 'C',
+            9 => 'D',
+            4 => 'E',
+            5 => 'F',
+            6 => 'G'
         );
 
         // Set content from data
